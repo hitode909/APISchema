dbcae45962955c6782c4b28d6e6fef8b1cb2235d
hitode909 <hitode909@gmail.com>
2015-08-24 19:13:47 +0900

    separate validator class

---
+++ lib/APISchema/Validator/Decoder.pm
@@ -0,0 +1,26 @@
+package APISchema::Validator::Decoder;
+use strict;
+use warnings;
+
+# cpan
+use JSON::XS qw(decode_json);
+use URL::Encode qw(url_params_mixed);
+use Class::Accessor::Lite ( new => 1 );
+
+sub perl {
+    my ($self, $body) = @_;
+    return $body;
+}
+
+my $JSON = JSON::XS->new->utf8;
+sub json {
+    my ($self, $body) = @_;
+    return $JSON->decode($body);
+}
+
+sub url_parameter {
+    my ($self, $body) = @_;
+    return url_params_mixed($body, 1);
+}
+
+1;
